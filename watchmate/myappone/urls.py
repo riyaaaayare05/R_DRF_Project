c016a9214e432a34ec687bb52ef2793f681b44b6
riyaaaayare05 <127780681+riyaaaayare05@users.noreply.github.com>
2023-08-26 15:26:11 +0530

    commit:5

--- watchmate/myappone/urls.py
+++ watchmate/myappone/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
 from . import views
 
-urlpatterns = [
-    path('list/', views.movie_list, name='movie-list'),
-    path('<int:pk>/', views.movie_details, name='movie-detail'),
-]
+# urlpatterns = [
+#     path('list/', views.movie_list, name='movie-list'),
+#     path('<int:pk>/', views.movie_details, name='movie-detail'),
+# ]
+
+
 
--- watchmate/myappone/views.py
+++ watchmate/myappone/views.py
@@ -1,33 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from myappone.models import Movie
+# from django.shortcuts import render
+# from django.http import HttpResponse, JsonResponse
+# from myappone.models import Movie
 
-# Create your views here.
+# # Create your views here.
+
+# # def movie_list(request):
+# #     movies = Movie.objects.all()
+# #     return HttpResponse(movies.values())
+
+# #--------------------------------------------
 
 # def movie_list(request):
 #     movies = Movie.objects.all()
-#     return HttpResponse(movies.values())
 
-#--------------------------------------------
+#     data = {
+#         'movies':list(movies.values())
+#     }
 
-def movie_list(request):
-    movies = Movie.objects.all()
+#     return JsonResponse(data)
 
-    data = {
-        'movies':list(movies.values())
-    }
+# #--------------------------------------------
 
-    return JsonResponse(data)
+# def movie_details(request, pk):
+#     movie = Movie.objects.get(pk=pk)
 
-#--------------------------------------------
+#     data = {
+#         'movie':movie.name,
+#         'description':movie.description,
+#         'active':movie.activate
+#     }
 
-def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-
-    data = {
-        'movie':movie.name,
-        'description':movie.description,
-        'active':movie.activate
-    }
-
-    return JsonResponse(data)
+#     return JsonResponse(data)
